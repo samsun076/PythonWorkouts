7335cce1558caa928e1ef694fc2b4e1d016193f5
DM <dmarcino@cswg.com>
2022-03-07 16:02:03 -0500

    sync

---
+++ 04_hex_output.py
@@ -0,0 +1,5 @@
+"""
+wirte a function called (hex_output) that takes a hex number and returns the decimal equivalent
+if a user enters 50, youll assum its a hex equal to 0x50 and will return a value of 80
+
+"""
