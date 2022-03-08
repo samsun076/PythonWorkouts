621288fcd65128b75964bf7f2375f986901b79ba
Dave Marcinowski <davemarcinowski@gmail.com>
2022-03-08 08:02:38 -0500

    sync

--- 04_hex_output.py
+++ 04_hex_output.py
@@ -3,3 +3,16 @@ wirte a function called (hex_output) that takes a hex number and returns the dec
 if a user enters 50, youll assum its a hex equal to 0x50 and will return a value of 80
 
 """
+def hex_output(hexnum):
+    total = 0
+    for index, num in enumerate(reversed(hexnum)):
+        total += int(num, 16) * 16 ** index  #enter second option argument in int function
+        print(f'{num} * 16 ** {index}: {total}')
+    print()
+
+
+
+hex_output('1357')
+hex_output('ff')
+hex_output('3eafd')
+
