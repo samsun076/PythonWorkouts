6ccfe388e4fa6e5f6d35a77a9154e8472e6b1794
DM <dmarcino@cswg.com>
2022-03-09 15:33:12 -0500

    sync

---
+++ 05_pig_latin.py
@@ -0,0 +1,36 @@
+"""
+Write a function called pig_latin that takes a string as input ( assumed to be english)
+The function will return the translation of the string in pig latin
+
+assum no capital letters or punctuation.
+
+If the word begins with a vowel (a, e, i, o, or u), add “way” to the end of the word. So “air” becomes “airway” and “eat” becomes “eatway.”
+
+If the word begins with any other letter, then we take the first letter, put it on the end of the word, and then add “ay.” Thus, “python” becomes “ythonpay” and “computer” becomes “omputercay.”
+"""
+# Here I did not follow the requirments and added the functionality of a string which turns out is 
+# exercise6.
+# MY SOLUTION
+
+def pig_latin():
+    my_string = input("Enter a sting to convert into Pig Latin: ")
+    vowels = 'aeiou'
+    pig_string = ''
+    for word in my_string.split():
+        if word[0] in vowels:
+            pig_string += word + 'ay '
+        else:
+            pig_string += word[1:] + word[0] + 'ay '
+    return pig_string
+
+
+# BOOK SOLUTION
+def pig_latin2(phrase):
+    if phrase[0] in 'aeiou':
+        return f'{phrase}way'
+    return f'{phrase[1:]}{phrase[0]}ay'
+
+print(pig_latin())
+print()
+print(pig_latin2("another"))
+print(pig_latin2("david"))
