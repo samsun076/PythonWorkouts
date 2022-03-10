07f0736c5659d2e4d9f9bfa07c7bd871cdefecfa
DM <dmarcino@cswg.com>
2022-03-10 08:17:33 -0500

    sync

--- 05a_pig_latin.py
+++ 05a_pig_latin.py
@@ -3,10 +3,22 @@ import string
 
 
 def pig_latin(word):
-    if word[0] in 'aeiou':
-        return f'{word}way'
+    if word[0] in 'aeiouAEIOU':
+        output = f'{word}way'
     else:
-        return f'{word[1:]}{word[0]}ay'
+        output =  f'{word[1:]}{word[0]}ay'
+
+    if word[0] in string.ascii_uppercase:
+        output = output.capitalize()
+    return output
+
+
+################################3
 
 print(pig_latin('elephant'))
 print(pig_latin('dave'))
+print(pig_latin('wim'))
+print()
+print(pig_latin('Wim'))
+print(pig_latin('Elephant'))
+print(pig_latin('Dave'))
