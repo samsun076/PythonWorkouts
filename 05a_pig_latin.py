1e7a533c1691daa7388beb80c3f96f89960817dc
Dave Marcinowski <davemarcinowski@gmail.com>
2022-03-10 07:44:02 -0500

    sync

--- 05a_pig_latin.py
+++ 05a_pig_latin.py
@@ -1,1 +1,12 @@
+import string
 """Solution to chapter 2, exercise 5, beyond 1: Pig Latin, handle capitalization"""
+
+
+def pig_latin(word):
+    if word[0] in 'aeiou':
+        return f'{word}way'
+    else:
+        return f'{word[1:]}{word[0]}ay'
+
+print(pig_latin('elephant'))
+print(pig_latin('dave'))
