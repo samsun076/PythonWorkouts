b4478420d35da06c1b577008e0aa82eb4cb63be7
DM <dmarcino@cswg.com>
2022-03-14 16:02:42 -0400

    sync

--- 07a_ubbi_Capitalized.py
+++ 07a_ubbi_Capitalized.py
@@ -1,5 +1,8 @@
+import string
 """
 you’ll write a function (called ubbi_dubbi) that takes a single word (string) as an argument. It returns a string, the word’s translation into Ubbi Dubbi. So if the function is called with octopus, the function will return the string uboctubopubus. And if the user passes the argument elephant, you’ll output ubelubephubant.
+
+Add logic to handle capitalized words.
 """
 
 
@@ -10,11 +13,18 @@ def ubbi(word):
             ubbi.append(f'ub{letter}')
         else:
             ubbi.append(letter)
-    return ''.join(ubbi)
+        #if word[0] in string.ascii_uppercase:
+        #    ubbi[0] = ubbi[0].capitalize()
+        return ''.join(ubbi)
 
 
+
+# Capitalize solution not working
+# This is the working solution from the book
+#
 print(ubbi('octopus'))
 print(ubbi('elephant'))
+print(ubbi('Dave'))
 print()
 print(ubbi('octopus') == 'uboctubopubus')
 print(ubbi('elephant') == 'ubelubephubant')
