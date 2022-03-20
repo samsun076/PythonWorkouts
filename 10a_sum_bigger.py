f258e28b5356bf025b4540bc58bc9be333d1fd01
Dave Marcinowski <davemarcinowski@gmail.com>
2022-03-20 09:47:44 -0400

    sync

--- 10a_sum_bigger.py
+++ 10a_sum_bigger.py
@@ -2,10 +2,18 @@
 Write a function, mysum_bigger_than, that works the same as mysum, except that it takes a first argument that precedes *args. That argument indicates the threshold for including an argument in the sum. Thus, calling mysum_bigger _than(10, 5, 20, 30, 6) would return 50—because 5 and 6 aren’t greater than 10. This function should similarly work with any type and assumes that all of the arguments are of the same type. Note that > and < work on many different types in Python, not just on numbers; with strings, lists, and tuples, it refers to their sort order.
 """
 
-def mysum_bigger_than(num, *args):
-    print(f'your num: {num} and ARGS: {args}')
+def mysum_bigger_than(threshold, *numbers):
+    print(f'your threshold: {threshold} and adding numbers: {numbers}')
+    if not numbers:
+        return numbers
+    total = 0
+    for number in numbers:
+        if number > threshold:
+            total += number
+            print(number)
+    return total
 
 
 
-mysum_bigger_than(60, [50,60,70,80,90,100])
+print(mysum_bigger_than(60,50,60,70,80,90,100))
 
