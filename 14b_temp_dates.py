e67666fc7a15dbb3d7a25cfea3aaa2c155e84496
Dave Marcinowski <davemarcinowski@gmail.com>
2022-03-27 20:20:12 -0400

    sync

--- 14b_temp_dates.py
+++ 14b_temp_dates.py
@@ -1,20 +1,49 @@
 '''
-Define a dict whose keys are dates (represented by strings) from the most recent week 
+Define a dict whose keys are dates (represented by strings) from the most recent week - YYYY-MM-DD format
 and whose values are temperatures. 
 Ask the user to enter a date, and display the temperature on that date, as well as the previous and subsequent dates, if available.
 '''
-
-DATES = {'03/01/2022': 44,
-         '03/02/2022': 33,
-         '03/03/2022': 65,
-         '03/04/2022': 55,
-         '03/05/2022': 54,
-         '03/06/2022': 53,
-         '03/07/2022': 37,
+from datetime import datetime, timedelta
+TEMPS= {'2022-03-01': 44,
+         '2022-03-02': 33,
+         '2022-03-03': 65,
+         '2022-03-04': 55,
+         '2022-03-05': 54,
+         '2022-03-06': 53,
+         '2022-03-07': 37,
 }
 
 
-def get_temp()
-    pass
+def get_temp():
+    while True:
+        user_date = input("Enter a date in YYYY-MM-DD format: ").strip() 
+
+        if not user_date:
+            print("ABORTING")
+            break
+        if len(user_date) != 10:
+            print("format incorrect, try again: ")
+            continue
+
+        if user_date.count('-') != 2:
+            print("Please use the correct delimter: ")
+            continue
+
+        if user_date not in TEMPS:
+            print(f'{user_date} is invalid, try a different one: ')
+            continue
+
+        try:
+            date = datetime.fromisoformat(user_date).date()
+        except ValueError as e:
+            print("Not a valid date string, try again: ")
+            continue
+        previous_day = str(date + timedelta(1))
+        next_day = str(date - timedelta(1))
+     
+        print(f'\nprevious date: {previous_day} -- TEMP: {TEMPS.get(previous_day, "no date entry exists")}')
+        print(f'reqested date: {date} -- TEMP: {TEMPS[str(date)]}')
+        print(f'next date: {next_day} -- TEMP: {TEMPS.get(next_day, "No date entry exists")}\n')
 
 get_temp()
+
---
+++ 14c_birthday_days_old.py
@@ -0,0 +1,19 @@
+"""
+Define a dict whose keys are names of people in your family, 
+and whose values are their birth dates, as represented by Python date objects (http://mng.bz/ jggr). 
+Ask the user to enter the name of someone in your family, 
+and have the program calculate how many days old that person is.
+"""
+
+FAMILY = {'Leslie Bebb': '1979-09-02',
+          'Parker Marcinowski': '2012-04-08',
+          'Dave Marcinowski': '1980-04-03',
+          'Pickles': '2021-02-15',
+          'Sunny': '2019-06-06',
+        }
+
+def day_old():
+    pass
+
+
+days_old()
